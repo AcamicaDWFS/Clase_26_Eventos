f5c0b479d42d393132d1a0701a9e18bce32da4ae
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2020-07-09 20:31:09 -0500

    Optional challenged completed.

--- Challenge/Emmanuel_Hernandez_Lira/script.js
+++ Challenge/Emmanuel_Hernandez_Lira/script.js
@@ -3,5 +3,17 @@ const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
   const name = textInput.value;
-  alert(`¡Hola, ${name}!`);
+
+  if (!name) {
+    textInput.style.backgroundColor = "red";
+    alert("El input está vacío.");
+  } else {
+    alert(`¡Hola, ${name}!`);
+  }
+});
+
+textInput.addEventListener("keypress", () => {
+  if (textInput.style.backgroundColor === "red") {
+    textInput.style.backgroundColor = "white";
+  }
 });
