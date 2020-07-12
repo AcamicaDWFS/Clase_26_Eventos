b4ef5cd2e932250d7c3917f7a465f9bf16031b96
Estefanía Cervantes Lara <celara91@gmail.com>
2020-07-12 17:42:53 -0500

    Actualización del challenge 26

--- Challenge/ch26.js
+++ Challenge/ch26.js
@@ -1,2 +1,30 @@
-let input = document.getElementById("inputName").innerHTML;
-console.log(input);
+let inputN = document.getElementById("inputName");
+let btn = document.getElementById("btn");
+
+//Function arrow
+btn.addEventListener("click", () => {
+    
+    if (inputN.value == undefined || inputN.value == "") {
+        alert("El input está vacío. Escribe tu nombre por favor.");
+        inputN.style.backgroundColor = "red";
+    } else {
+        alert("Hola " + inputN.value);
+        inputN.value = "";
+    }
+});
+
+//Función para el color de la caja
+inputN.addEventListener("keydown", () => {
+    
+    inputN.style.background = "white";
+
+})
+
+//EventListener con function
+/* btn.addEventListener("click", function() {
+    let inputN = document.getElementById("inputName");
+    alert("Hola " + inputN.value);
+    inputN.value = "";
+}); */
+
+
